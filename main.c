aa412deae5dfad30b64936744f9b655eeee26d72
Jaehwan Kim <59593345+robinjh@users.noreply.github.com>
2022-10-16 19:04:29 +0900

    Update main.c

--- main.c
+++ main.c
@@ -5,8 +5,8 @@ void LSMprofile() {
     printf("이메일 : runring11@gmail.com\n");
     printf("학과 : 컴퓨터공학과\n");
     printf("학년 : 1\n");
-
 }
+
 void INOUT_KJH() {
 	int a, b, c;
 	int add = 0;
@@ -19,11 +19,13 @@ void INOUT_KJH() {
 	printf("subtracted result is %d\n", sub);
 	printf("written by KJH(robinjh0314@gmail.com, Computer Science and Engineering, 1st grade)THX!\n");
 }
+
 void CJW()
 {
   printf("NAME : choi jin woo\n");
   printf("Student ID : 2022040018\n");
 }
+
 int main () {
 printf("\n");
 LSMprofile();
