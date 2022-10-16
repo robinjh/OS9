014e276d60bb9abcf431172c319daef1241d3696
dydtmddl <107300027+dydtmddl@users.noreply.github.com>
2022-10-16 18:42:47 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
+
+void LSMprofile() {
+    printf("이름 : dydtmddl\n");
+    printf("이메일 : runring11@gmail.com\n");
+    printf("학과 : 컴퓨터공학과\n");
+    printf("학년 : 1");
+
+}
+
 int main () {
 printf("\n");
 printf("\n");
