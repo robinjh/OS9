4778a15e844a40943cb9618f26656ef058ad5198
Jaehwan Kim <59593345+robinjh@users.noreply.github.com>
2022-10-16 18:46:33 +0900

    Update main.c

--- main.c
+++ main.c
@@ -7,6 +7,18 @@ void LSMprofile() {
     printf("학년 : 1\n");
 
 }
+void INOUT_KJH() {
+	int a, b, c;
+	int add = 0;
+	int sub = 0;
+	printf("type three numbers, A B C \n(like 1 2 3)\n");
+	scanf("%d %d %d", &a, &b, &c);
+	add = a + b + c;
+	sub = a - b - c;
+	printf("added result is %d\n", add);
+	printf("subtracted result is %d\n", sub);
+	printf("written by KJH(robinjh0314@gmail.com, Computer Science and Engineering, 1st grade)THX!\n");
+}
 
 int main () {
 printf("\n");
