a16b229fb1ef310660af65d4e0f809a657bc0fa9
robinjh <robinjh0314@gmail.com>
2022-11-14 19:07:12 +0900

    Visual studio 로 먼저 작성한 내용, 간단한 메뉴 선택까지 구성되어 있음 추후 file 받아오는 방식 설정할 필요 있음

--- main.c
+++ main.c
@@ -1,46 +1,68 @@
 #include <stdio.h>
-
-void LSMprofile() {
-    printf("이름 : dydtmddl\n");
-    printf("이메일 : runring11@gmail.com\n");
-    printf("학과 : 컴퓨터공학과\n");
-    printf("학년 : 1\n");
+#include <Windows.h>
+//현재 함수부 전반적으로 정상 작동 하지 않고 있어 사용 중단
+void MENUE(int *n) {
+	printf("Welcome to ENIGMA!\n\n");
+	printf("MENU\n\n");
+	printf("1. Encript File(File will be compressed first!)\n");
+	printf("2. Encript Text\n");
+	printf("Enter the number of menue(1 or 2): ");
+	scanf("%d",&n);
 }
-
-void INOUT_KJH() {
-	int a, b, c;
-	int add = 0;
-	int sub = 0;
-	printf("type three numbers, A B C \n(like 1 2 3)\n");
-	scanf("%d %d %d", &a, &b, &c);
-	add = a + b + c;
-	sub = a - b - c;
-	printf("added result is %d\n", add);
-	printf("subtracted result is %d\n", sub);
-	printf("written by KJH(robinjh0314@gmail.com, Computer Science and Engineering, 1st grade)THX!\n");
+void EF(int n) {
+  //이 함수가 추후 File 암호화로 진행될 것
+	printf("파일 암호화 결과");
+	Sleep(1000);
 }
-
-void CJW()
-{
-  printf("NAME : choi jin woo\n");
-  printf("Student ID : 2022040018\n");
+void ET(int n) {
+  //이 함수가 추후 문자열 암호화로 진행될 것
+	printf("텍스트 암호화 결과");
+	Sleep(1000);
 }
-
-void kyt()
-{
-	printf("name: kim yun tae");
-	printf("height: 173cm");
-	printf("weight: 60kg");
+void CHOOSE(int n, int flag) {
+	switch (n) {
+	case 1: EF(n); break;
+	case 2: ET(n); break;
+	default: printf("PLZ reenter the number of the menue : ");
+		printf("%d", n);
+		scanf("%d", &n);
+		if (n == 1 || n == 2) {
+			flag++;
+		}
+		break;
+	}
 }
+int main() {
+	int n = 0;
+	int flag = 1;
+	//MENUE(n);//현재 에러 발생으로 사용 중지
+	printf("Welcome to ENIGMA!\n\n");
+	printf("MENU\n\n");
+	printf("1. Encript File(File will be compressed first!)\n");
+	printf("2. Encript Text\n");
+	printf("Enter the number of menue(1 or 2): ");
+	scanf("%d", &n);
+	while (flag == 1)
+	{
+		if (n == 1 || n == 2) {
+			flag++;
+		}
+		switch (n) {
+		case 1: {system("cls"); EF(n); break; }
+		case 2: {system("cls"); ET(n); break; }
+		default: printf("PLZ reenter the number of the menue : ");
+			scanf("%d", &n); break;
 
-int main () {
-printf("\n");
-LSMprofile();
-printf("\n");
-INOUT_KJH();
-printf("\n");
-CJW();
-printf("\n");
-kyt();
-  return 0;
+		}
+
+	}
+	system("cls");
+	printf("THX for your usage!\n");
+	printf("If you faced any bug, PLZ report : robinjh0314@gmail.com");
+
+	Sleep(1000);
+
+	system("cls");
+
+	return 0;
 }
