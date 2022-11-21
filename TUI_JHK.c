69002f5ab7ecf852a3a1dac60d2e0bede8ecfda3
robinjh <robinjh0314@gmail.com>
2022-11-21 22:46:19 +0900

    최종적으로 기능 더 추가함, 현재 커럽션이 뜨는 오류가 있음

--- TUI_JHK.c
+++ TUI_JHK.c
@@ -1,109 +1,160 @@
 //여기서 추후 코드 개발할 것!
-<<<<<<< HEAD
-#include <stdio.h>
-#include <Windows.h>
-//현재 함수부 전반적으로 정상 작동 하지 않고 있어 사용 중단
-void MENUE(int *n) {
-=======
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <Windows.h>
 #define MAX 1000
 
-int MENUE(int *n) {
->>>>>>> fce90095d7538d16a64315153eff9a9ff591d27f
+int MENUE(int* n) { //실행 시 처음으로 나오는 메뉴를 보여주는 함수
 	printf("Welcome to ENIGMA!\n\n");
 	printf("MENU\n\n");
-	printf("1. Encript File(File will be compressed first!)\n");
-	printf("2. Encript Text\n");
-<<<<<<< HEAD
-	printf("Enter the number of menue(1 or 2): ");
-	scanf("%d",&n);
-}
-void EF(int n) {
-  //이 함수가 추후 File 암호화로 진행될 것
-	printf("파일 암호화 결과");
-	Sleep(1000);
-}
-void ET(int n) {
-  //이 함수가 추후 문자열 암호화로 진행될 것
-	printf("텍스트 암호화 결과");
-	Sleep(1000);
-}
-void CHOOSE(int n, int flag) {
-	switch (n) {
-	case 1: EF(n); break;
-	case 2: ET(n); break;
-	default: printf("PLZ reenter the number of the menue : ");
-		printf("%d", n);
-		scanf("%d", &n);
-		if (n == 1 || n == 2) {
-			flag++;
-		}
-		break;
-	}
-}
-int main() {
-	int n = 0;
-	int flag = 1;
-	//MENUE(n);//현재 에러 발생으로 사용 중지
-	printf("Welcome to ENIGMA!\n\n");
-	printf("MENU\n\n");
-	printf("1. Encript File(File will be compressed first!)\n");
-	printf("2. Encript Text\n");
-	printf("Enter the number of menue(1 or 2): ");
-	scanf("%d", &n);
-	while (flag == 1)
-	{
-		if (n == 1 || n == 2) {
-			flag++;
-		}
-		switch (n) {
-		case 1: {system("cls"); EF(n); break; }
-		case 2: {system("cls"); ET(n); break; }
-		default: printf("PLZ reenter the number of the menue : ");
-			scanf("%d", &n); break;
-
-		}
-
-	}
-=======
+	printf("1. Encript / Decript File(File will be compressed first!)\n");
+	printf("2. Encript / Decript Text\n");
 	printf("3. Encription Key management\n");
 	printf("Enter the number of menue( 1 - 3 ): ");
-	scanf("%d",&n);
+	scanf("%d", &n);
 	return n;
-
 }
-//void REISSUE(){	} 이 함수가 키 발급 관련 함수
-//void TXTENCRIPT(*TEXT){	} 이 함수가 문자열 암호화 해줄 함수
-//void FILEENCRIPT(*FNAME) {	} 이 함수가 파일 암호화 해줄 함수, file 주소가 입력될 것
-void EF() {
+
+void FILEENCRIPT() {	//실질적으로 파일 암호화가 되서 돌아갈 부분, 이름 받아올거임
 	char FNAME[MAX];
 	printf("You choosed Encriping file\n");
-	printf("Put the file in same location with program and enter the name of file \n(Ex : file.txt )\n >>> ");
+	printf("Put the file in same location with program and enter the name of file \n(Ex : file.txt )\n >>>   ");
 	scanf("%s", &FNAME);
+
+	FILE* FOPEN = fopen( FNAME, "r");
+	if (FOPEN == NULL) {
+		printf("There is no file! PLZ check again!");
+		Sleep(1000);
+		system("cls");
+	}
+	fclose(FOPEN);
+	printf("File Encripted! Check RESULT.txt");
+	FILE* RESULT = fopen( "RESULT.txt", "w");
+	fprintf(RESULT, "%d ", FNAME);
+	fclose(RESULT);
 	printf("%s", FNAME);
-
-	//FILEENCRIP(LOCATION)
-	printf("파일 암호화 결과"); //암호화 된 파일의 위치를 보여줄 것, 이 프로그램이 저장되어 있는 file 안에 저장될거임
 	Sleep(10000);
+	system("cls");
 }
-void ET() {
+
+void FILEDECRIPT() { // 파일 복호화 해주는 기능을 할 함수
+	char FNAME[MAX];
+	printf("You choosed Decriping file\n");
+	printf("Put the file in same location with program and enter the name of file \n(Ex : file.txt )\n >>>   ");
+	scanf("%s", &FNAME);
+	FILE* FOPEN =fopen( FNAME, "r");
+	if (FOPEN == NULL) {
+		printf("There is no file! PLZ check again!");
+		Sleep(5000);
+		system("cls");
+	}
+	printf("%s", FNAME);//이거는 복호화 한 파일이 나가는 자리가 될것
+	fclose(FOPEN);
+}
+
+void EDF() { //file 관련 처리 담당할 함수
+	char CHECK;
+	int temp = 0;
+	printf("Encription or Decription? [E / D]\n>>>>   ");
+	scanf("%s", &CHECK);
+	int flag = 1;
+	do {
+		if (CHECK == 'E' || CHECK == 'e' || CHECK == 'D' || CHECK == 'd')
+		{
+			flag++;
+			if (CHECK == 'E' || CHECK == 'e')
+			{
+				temp = 1;
+			}
+			if (CHECK == 'D' || CHECK == 'd')
+			{
+				temp = 2;
+			}
+		}
+		switch (temp) {
+		case 1: {system("cls"); FILEENCRIPT(); break; }
+		case 2: {system("cls"); FILEDECRIPT(); break; }
+		default: printf("PLZ reenter the menue : ");
+			scanf("%s", &CHECK); break;
+		}
+
+	} while (flag == 1);
+}
+
+void TXTENCRIPT() { // 이 함수가 문자열 암호화 해줄 함수
 	char TEXT[MAX];
-	printf("You choosed Encripting text\n");
-	printf("Enter the text : ");
+	printf("You choosed Encriping text\n");
+	printf("Type text!\n >>>   ");
 	scanf("%s", &TEXT);
-	printf("%s", TEXT);
-
-	//TXTENCRIPT(TEXT);
-	printf("텍스트 암호화 결과"); //실시간으로 출력해주지 않고 TXT 파일 형태로 제공할 것
+	printf("Encripting finished! The result is %s\n", TEXT);
+	printf("You can also find result at RESULT.txt");
+	FILE* RESULT =fopen("RESULT.txt", "w");
+	fprintf(RESULT, "%d ", TEXT);
+	fclose(RESULT);
 	Sleep(10000);
+	system("cls");
 }
+
+
+void TXTDECRIPT() { // 이 함수가 문자열 복호화 해 줄 함수
+	char TEXT[MAX];
+	printf("You choosed Decriping text\n");
+	printf("Type text!\n >>>   ");
+	scanf("%s", &TEXT);
+	printf("Encripting finished! The result is &s\n", TEXT);
+
+	printf("You can also find result at RESULT.txt");
+	FILE* RESULT = fopen("RESULT.txt", "w");
+	fprintf(RESULT, "%d ", TEXT);
+	fclose(RESULT);
+	printf("%s", TEXT);
+	Sleep(10000);
+	system("cls");
+}
+
+void EDT() { //텍스트 관련 처리 담당할 함수
+	char CHECK2;
+	printf("Encription or Decription? [E / D]\n>>>>   ");
+	scanf("%c", &CHECK2);
+	int flag = 1;
+	int temp = 0;
+	do {
+		if (CHECK2 == 'E' || CHECK2 == 'e' || CHECK2 == 'D' || CHECK2 == 'd')
+		{
+			flag++;
+			if (CHECK2 == 'E' || CHECK2 == 'e')
+			{
+				temp = 1;
+			}
+			if (CHECK2 == 'D' || CHECK2 == 'd')
+			{
+				temp = 2;
+			}
+		}
+		switch (temp) {
+		case 1: {system("cls"); TXTENCRIPT(); break; }
+		case 2: {system("cls"); TXTDECRIPT(); break; }
+		default: printf("PLZ reenter the menue : ");
+			scanf("%s", &CHECK2); break;
+		}
+
+	} while (flag == 1);
+}
+
+void REISSUE() { // 이 함수가 키 재발급 해줄 함수임
+	FILE* PKEY = fopen("publickey.txt", "w");
+	//fprintf();
+	fclose(&PKEY);
+	printf("Sucsessfully reissued!");
+	Sleep(2500);
+	system("cls");
+
+}
+
 void KEY() {
 	int KEY;
 	char answer;
-	FILE* PKEY = NULL;
-	fopen_s(&PKEY, "publickey.txt", "r");
+	FILE* PKEY =fopen("publickey.txt", "r");
 	if (PKEY == NULL) {
 		printf("You dont have public key! Issue it first!");
 		Sleep(10000);
@@ -111,29 +162,31 @@ void KEY() {
 		return -1;
 	}
 	fscanf_s(PKEY, "%d", &KEY);
-	printf("This is Your Public Key : %d\n",KEY);
+	fclose(PKEY);
+	printf("This is Your Public Key : %d\n", KEY);
 	printf("Do you wnat to reissue your public and private key? \n");
 	printf("YOUR CURRENT KEY WILL BE DELETED PERMENENTLY! [Y/N]\n");
 	scanf("%s", &answer);
 	if (answer == 'Y') {
 		system("cls");
 		printf("공개 키 재발급");
-		//REISSUE();
+		REISSUE();
 		Sleep(10000);
 		system("cls");
 		return 0;
 	}
 }
-int CHOOSE(int *n){
+
+int CHOOSE(int* n) {
 	int flag = 1;
 	int temp = n;
-	do{
-		if (temp == 1 || temp == 2 || temp ==3) {
+	do {
+		if (temp == 1 || temp == 2 || temp == 3) {
 			flag++;
 		}
 		switch (temp) {
-		case 1: {system("cls"); EF(); break; }
-		case 2: {system("cls"); ET(); break; }
+		case 1: {system("cls"); EDF(); break; }
+		case 2: {system("cls"); EDT(); break; }
 		case 3: {system("cls"); KEY(); break; }
 		default: printf("PLZ reenter the number of the menue : ");
 			scanf("%d", &temp); break;
@@ -143,11 +196,19 @@ int CHOOSE(int *n){
 	} while (flag == 1);
 }
 int main() {
+
+	FILE* PKEY = NULL;
+	fopen_s(&PKEY, "publickey.txt", "r");
+	if (PKEY == NULL) {
+		fopen_s(&PKEY, "publickey.txt", "w");
+		//fprintf();
+		fclose(&PKEY);
+	}
+
 	int n = 0;
-	n=MENUE(n);
+	n = MENUE(n);
 	CHOOSE(n);
 
->>>>>>> fce90095d7538d16a64315153eff9a9ff591d27f
 	system("cls");
 	printf("THX for your usage!\n");
 	printf("If you faced any bug, PLZ report : robinjh0314@gmail.com");
@@ -156,9 +217,6 @@ int main() {
 
 	system("cls");
 
-<<<<<<< HEAD
-=======
 
->>>>>>> fce90095d7538d16a64315153eff9a9ff591d27f
 	return 0;
 }
