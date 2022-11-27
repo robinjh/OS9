0426ca5a31951da037be32679ec018950bd98536
robinjh <robinjh0314@gmail.com>
2022-11-27 23:52:13 +0900

    병합중, file encription 에서 함수 가져와서 합치는 중

--- TUI_JHK.c
+++ TUI_JHK.c
@@ -1,10 +1,261 @@
 //여기서 추후 코드 개발할 것!
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <stdlib.h>
-#include <Windows.h>
 #include <string.h>
-#define MAX 1000
+#include <windows.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#define MAX 10000
+int PRM1, PRM2;   //소수 2개
+int mul;      //p*q
+int Euler;
+int PubKEY;      //공개키e
+int ScrKEY = 1;  //비밀키d
+
+void select_PRMs(int* PRM1, int* PRM2) {         //소수 2개를 선택하는 함수
+    int i, j;
+    srand((unsigned)time(NULL));
+
+    i = (rand() % 80) + 1;
+    for (i; 1; i++) {
+        for (j = 2; j <= i; j++) {
+            if (i % j == 0)
+                break;
+        }
+        if (j == i) {
+            *PRM1 = i;
+            break;
+        }
+    }
+    i = (rand() % 80) + 1;
+    for (i; 1; i++) {
+        for (j = 2; j <= i; j++) {
+            if (i % j == 0)
+                break;
+        }
+        if (j == i) {
+            *PRM2 = i;
+            break;
+        }
+    }
+
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//RSA의 공개키, 비공개키를 만들어 보여주는 함수
+
+void keymaking() {
+
+    select_PRMs(&PRM1, &PRM2);
+    mul = PRM1 * PRM2;
+    Euler = (PRM1 - 1) * (PRM2 - 1);
+
+    for (ScrKEY; 1; ScrKEY++) {
+        PubKEY = (rand() % (Euler - 2)) + 2;  // 1 < e < Euler(n)
+        int result;
+        result = (ScrKEY * PubKEY) % Euler;
+        if (result == 1) {
+            break;
+        }
+    }
+    printf("공개키e, n (%d, %d)\n비밀키d, n (%d, %d)", PubKEY, mul, ScrKEY, mul);
+
+}
+
+//값, 키를 받아 RSA암호화를 진행하는 함수
+
+long pow_(long i, long j, long k) {
+    double l, temp, p = 1;
+    for (temp = 0; temp < j; temp++) {
+        p = (p * ((double)i));
+        l = (long)(p / k);
+        p = p - (l * k);
+    }
+    return (long)p;
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////이 사이가 키 관리 함수에 포함되야 될 것
+
+void EDF()
+{
+	int choice;
+   for (;;)
+   {
+      printf("\n1. 파일 암호화\n2. 파일 복호화\n5. 종료\n\n");
+	  printf("입력 : ");
+	  scanf_s("%d", &choice);
+	  if (choice < 3) { break; }
+	  else { continue; }
+   }
+
+   switch (choice)
+   {
+   case 1:
+      FileEncoding();
+      break;
+
+   case 2:
+      FileDecoding();
+      break;
+
+   case 3:
+      exit(1);
+   }
+}
+
+void FileEncoding()
+
+{
+
+   FILE* FileCreat = NULL;
+
+   int File;
+
+   char Type[27];
+
+   system("cls");
+
+   printf("※ 확장자 까지 입력하여 주셔야 합니다.\n");
+
+   getchar();
+
+   for (;;)
+
+   {
+
+      printf("암호화 할 파일을 입력 해 주십시오 : ");
+
+      gets_s(Type);
+
+      FileCreat = fopen(Type, "rb");
+
+      if (FileCreat == NULL)
+
+      {
+
+         printf("\n파일이 존재하지 않습니다.\n");
+
+         continue;
+
+      }
+
+      else
+
+      {
+
+         FILE* CreatFile;
+
+         char NameType[27];
+
+         printf("\n생성 될 파일명을 적어주세요(.txt) : ");
+
+         gets_s(NameType);
+
+         CreatFile = fopen(NameType, "w");
+
+         while ((File = fgetc(FileCreat)) != EOF)
+
+         {
+
+            File = pow_(File, PubKEY, mul);
+
+            printf(".");
+
+            fprintf(CreatFile, "%d ", File);
+
+         }
+
+         fclose(CreatFile);
+
+         system("cls");
+         printf("\n\n암호화 완료! 시작 화면으로 돌아갑니다\n");
+
+         break;
+
+      }
+
+   }
+
+   fclose(FileCreat);
+
+}
+
+void FileDecoding()
+
+{
+
+   FILE* DeCodFileCreat = NULL;
+
+   int File;
+
+   char DeType[27];
+
+   system("cls");
+
+   printf("※ 확장자 까지 입력하여 주셔야 합니다.\n");
+
+   getchar();
+
+   for (;;)
+
+   {
+
+      printf("복호화 할 파일을 입력 해 주십시오(.txt) : ");
+
+      gets_s(DeType);
+
+      DeCodFileCreat = fopen(DeType, "r");
+
+      if (DeCodFileCreat == NULL)
+
+      {
+
+         printf("\n파일이 존재하지 않습니다.\n");
+
+         continue;
+
+      }
+
+      else
+
+      {
+
+         FILE* DeCodCreatFile;
+
+         char DeCodNameType[27];
+
+         printf("\n생성 될 파일명을 적어주세요 : ");
+
+         gets_s(DeCodNameType);
+
+         DeCodCreatFile = fopen(DeCodNameType, "wb");
+
+         while (fscanf(DeCodFileCreat, "%d", &File) != EOF)
+
+         {
+
+            File = pow_(File, ScrKEY, mul);
+
+            printf(".");
+
+            fwrite(&File, sizeof(char), 1, DeCodCreatFile);
+
+         }
+
+         fclose(DeCodCreatFile);
+
+         system("cls");
+         printf("\n\n복호화 완료! 시작 화면으로 돌아갑니다\n");
+
+         break;
+
+      }
+
+   }
+
+   fclose(DeCodFileCreat);
+
+}
+
 
 void MENUE() { //실행 시 처음으로 나오는 메뉴를 보여주는 함수
 	printf("Welcome to ENIGMA!\n\n");
@@ -55,7 +306,7 @@ void FILEDECRIPT() { // 파일 복호화 해주는 기능을 할 함수
 	//fclose(FOPEN);
 }
 
-void EDF() { //file 관련 처리 담당할 함수
+void EDFold() { //file 관련 처리 담당할 함수
 	char CHECK;
 	int temp = 0;
 	printf("Encription or Decription? [E / D]\n>>>>   ");
