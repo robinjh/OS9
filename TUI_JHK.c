d6ba6db347c782523a00b26b25afc46ef7aa3356
robinjh <robinjh0314@gmail.com>
2022-12-05 23:25:01 +0900

    문자열 암호화 담당 알고리즘 메인에 통합 및 원할히 상호작용 하도록 수정(UI 형식 통합, 암호화에 필요한 키 가져오기)

--- TUI_JHK.c
+++ TUI_JHK.c
@@ -1,4 +1,5 @@
 //여기서 추후 코드 개발할 것!
+//여기서 추후 코드 개발할 것!
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
@@ -7,6 +8,9 @@
 #include <math.h>
 #include <time.h>
 #define MAX 10000
+
+
+
 int PRM1, PRM2;   //소수 2개
 int mul;      //소수 곱
 int Euler;
@@ -14,6 +18,14 @@ int PubKEY;      //공개키e
 int ScrKEY = 1;  //비밀키d
 void CHOOSE();
 void newkeymaking();
+
+int i, n, X, Y;
+long int e[50], d[50], temp[50], j, m[50], en[50];
+char msg[MAX];
+void encrypt();
+void decrypt();
+
+void EDT();
 
 void select_PRMs(int* PRM1, int* PRM2) {         //소수 2개를 선택하는 함수
 	int i, j;
@@ -171,54 +183,109 @@ void MENUE() { //실행 시 처음으로 나오는 메뉴를 보여주는 함수
 	printf("4. End program\n");
 	printf("Enter the number of menue( 1 - 4 ): ");
 }
+////////////////////////////////////////////////////////////////////////////////////////////////
 
-void TXTENCRIPT() { // 이 함수가 문자열 암호화 해줄 함수
-	char TEXT[MAX];
-	printf("You choosed Encriping text\n");
-	printf("Type text!\n >>>   ");
-	scanf("%s", &TEXT);
-	printf("Encripting finished! The result is %s\n", TEXT);
-	printf("You can also find result at RESULT.txt");
-	FILE* RESULT = fopen("RESULT.txt", "w");
-	fprintf(RESULT, "%d ", TEXT);
-	fclose(RESULT);
-	Sleep(5000);
-	system("cls");
-}
-
-void TXTDECRIPT() { // 이 함수가 문자열 복호화 해 줄 함수
-	char TEXT[MAX];
-	printf("You choosed Decriping text\n");
-	printf("Type text!\n >>>   ");
-	scanf("%s", &TEXT);
-	printf("Encripting finished! The result is &s\n", TEXT);
-
-	printf("You can also find result at RESULT.txt");
-	FILE* RESULT = fopen("RESULT.txt", "w");
-	fprintf(RESULT, "%d ", TEXT);
-	fclose(RESULT);
-	printf("%s", TEXT);
-	Sleep(5000);
-	system("cls");
-}
-
-void EDT() { //텍스트 관련 처리 담당할 함수
-	char CHECK2;
-	printf("Encription or Decription? [E / D]\n>>>>   ");
-
-	scanf(" %c", &CHECK2);
-
-	int flag = 1;
-	int temp = 0;
-
-	while (flag == 1) {
-		if (CHECK2 == 'E') { flag++; system("cls");  TXTENCRIPT(); break; }
-		else if (CHECK2 == 'e') { flag++; system("cls"); TXTENCRIPT(); break; }
-		else if (CHECK2 == 'D') { flag++; system("cls"); TXTDECRIPT(); break; }
-		else if (CHECK2 == 'd') { flag++; system("cls"); TXTDECRIPT(); break; }
-		else { printf("PLZ reenter the menue : "); scanf(" %c", &CHECK2); }
+//function to encrypt the message
+void encrypt()
+{
+	long int pt, ct, key = e[0], k, len;
+	i = 0;
+	len = strlen(msg);
+	while (i != len)
+	{
+		pt = m[i];
+		pt = pt - 96;
+		k = 1;
+		for (j = 0; j < key; j++)
+		{
+			k = k * pt;
+			k = k % n;
+		}
+		temp[i] = k;
+		ct = k + 96;
+		en[i] = ct;
+		i++;
 	}
+	en[i] = -1;
+	printf("\n\nTHE ENCRYPTED MESSAGE IS\n");
+	for (i = 0; en[i] != -1; i++)
+		printf("%c", en[i]);
+}
 
+//function to decrypt the message
+void decrypt()
+{
+	long int pt, ct, key = d[0], k;
+	i = 0;
+	while (en[i] != -1)
+	{
+		ct = temp[i];
+		k = 1;
+		for (j = 0; j < key; j++)
+		{
+			k = k * ct;
+			k = k % n;
+		}
+		pt = k + 96;
+		m[i] = pt;
+		i++;
+	}
+	m[i] = -1;
+	printf("\n\nTHE DECRYPTED MESSAGE IS\n");
+	for (i = 0; m[i] != -1; i++)
+		printf("%c", m[i]);
+	printf("\n");
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+void EDT() {
+	{
+		int choice;
+		for (;;)
+		{
+			printf("\n1. Text Encrypt\n2. Text Decrypt\n3. Exit to main menue\n\n");
+			printf("Type : ");
+			scanf_s("%d", &choice);
+			system("cls");
+			if (choice < 4) { break; }
+			else { continue; }
+		}
+		switch (choice)
+		{
+		case 1:
+			printf("\nENTER MESSAGE OR STRING TO ENCRYPT\n");
+			getchar();
+			gets(msg);
+			for (i = 0; msg[i] != NULL; i++)
+				m[i] = msg[i];
+			encrypt();
+			printf("\nSave the result by copying text(Ctrl+c)");
+			Sleep(5000);
+			system("CLS");
+			EDT();
+			break;
+
+		case 2:
+			printf("\nENTER MESSAGE OR STRING TO ENCRYPT\n");
+			getchar();
+			gets(msg);
+			for (i = 0; msg[i] != NULL; i++)
+				m[i] = msg[i];
+			decrypt();
+			printf("\nSave the result by copying text(Ctrl+c)");
+			Sleep(5000);
+			system("CLS");
+			EDT();
+			break;
+
+		case 3:
+			MENUE();
+			CHOOSE();
+			break;
+		}
+
+	}
 }
 
 void keymaking() {
@@ -243,7 +310,7 @@ void newkeymaking() {                   // [추가한 부분] 3자리 수 이상
 	while (1) {
 
 		keymaking();
-		if (PubKEY >= 100 && ScrKEY >= 100 && PubKEY < 10000 && ScrKEY < 10000) {		//비밀키가 과도하게 크게 생성되는 문제가 있어 만 이하의 수로 결정되도록 조건 추가함
+		if (PubKEY >= 100 && ScrKEY >= 100 && PubKEY <= 10000 && ScrKEY <= 10000) {		//비밀키가 과도하게 크게 생성되는 문제가 있어 만 이하의 수로 결정되도록 조건 추가함
 
 			break;
 		}
@@ -339,6 +406,10 @@ int main() {
 		fscanf(PKEYOPEN, "%d %d %d", &PubKEY, &ScrKEY, &mul);
 		fclose(PKEYOPEN);
 	}
+
+	e[0] = PubKEY;
+	d[0] = ScrKEY;
+	n = mul;
 
 	MENUE();
 
