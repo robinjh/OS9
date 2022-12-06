446548d933c09d597823b524433d98d435ecd3dd
ediottchoi <106829273+ediottchoi@users.noreply.github.com>
2022-12-06 23:01:47 +0900

    Update rsa.c

--- rsa.c
+++ rsa.c
@@ -1,191 +1,145 @@
-#include <stdio.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <math.h>
-#include <time.h>
-#include <string.h>
-#include "rsa.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
 
-char buffer[1024];
-const int MAX_DIGITS = 50;
-int i,j = 0;
+int x, y, n, t, i, flag;
+long int e[50], d[50], temp[50], j, m[50], en[50];
+char msg[100];
+int prime(long int);
+void encryption_key();
+long int cd(long int);
+void encrypt();
+void decrypt();
 
-struct public_key_class{
-  long long modulus;
-  long long exponent;
-};
-
-struct private_key_class{
-  long long modulus;
-  long long exponent;
-};
-
-
-long long gcd(long long a, long long b)
+int main()
 {
-  long long c;
-  while ( a != 0 ) {
-    c = a; a = b%a;  b = c;
+  printf("\nENTER FIRST PRIME NUMBER\n");
+  scanf("%d", &x);
+  flag = prime(x);
+  if(flag == 0)
+  {
+    printf("\nINVALID INPUT\n");
+    exit(0);
   }
-  return b;
-}
-
-
-long long ExtEuclid(long long a, long long b)
-{
- long long x = 0, y = 1, u = 1, v = 0, gcd = b, m, n, q, r;
- while (a!=0) {
-   q = gcd/a; r = gcd % a;
-   m = x-u*q; n = y-v*q;
-   gcd = a; a = r; x = u; y = v; u = m; v = n;
-   }
-   return y;
-}
-static inline long long modmult(long long a,long long b,long long mod)
-{
-   
-   if (a == 0 ){
-         return 0;
-   }
-   register long long product = a * b;
-    
-   if (product / a == b){
-          return product % mod;
-   }
-   
-   if ( a & 1 ) {
-         product = modmult((a>>1), b, mod);
-         if ((product << 1) > product ){
-         return ((( product << 1 ) % mod ) + b) % mod;
-      }
-   }
-   
-   product = modmult((a >> 1), b, mod);
-   if ((product << 1) > product){
-         return (product << 1) % mod ;
-         }
-  
-    long long sum;
-    sum = 0;
-    while(b>0)
-    {
-        if(b&1)
-            sum = (sum + a) % mod;
-        a = (2*a) % mod;
-        b>>=1;
-    }
-    return sum;
-}
-long long rsa_modExp(long long b, long long e, long long m)
-{
-      long long product;
-      product = 1;
-      if (b < 0 || e < 0 || m <= 0){
-            return -1;
-      }
-      b = b % m;
-      while ( e > 0){
-            if (e & 1){
-                  product = modmult(product, b, m);
-            }
-            b = modmult(b, b, m);
-            e >>= 1;
-      }
-      return product;
-}
-
-
-long long *rsa_encrypt(const char *message, const unsigned long message_size,
-                     const struct public_key_class *pub)
-{
-  long long *encrypted = malloc(sizeof(long long)*message_size);
-  if(encrypted == NULL){
-    fprintf(stderr,
-     "Error: Heap allocation failed.\n");
-    return NULL;
+  printf("\nENTER SECOND PRIME NUMBER\n");
+  scanf("%d", &y);
+  flag = prime(y);
+  if(flag == 0 || x == y)
+  {
+    printf("\nINVALID INPUT\n");
+    exit(0);
   }
-  long long i = 0;
-  for(i=0; i < message_size; i++){
-    if ((encrypted[i] = rsa_modExp(message[i], pub->exponent, pub->modulus)) == -1)
-    return NULL;
-  }
-  return encrypted;
-}
+  printf("\nENTER MESSAGE OR STRING TO ENCRYPT\n");
 
-
-char *rsa_decrypt(const long long *message,
-                  const unsigned long message_size,
-                  const struct private_key_class *priv)
-{
-  if(message_size % sizeof(long long) != 0){
-    fprintf(stderr,
-     "Error: message_size is not divisible by %d, so cannot be output of rsa_encrypt\n", (int)sizeof(long long));
-     return NULL;
-  }
-  
- 
-  char *decrypted = malloc(message_size/sizeof(long long));
-  char *temp = malloc(message_size);
-  if((decrypted == NULL) || (temp == NULL)){
-    fprintf(stderr,
-     "Error: Heap allocation failed.\n");
-    return NULL;
-  }
-  
-  long long i = 0;
-  for(i=0; i < message_size/8; i++){
-    if ((temp[i] = rsa_modExp(message[i], priv->exponent, priv->modulus)) == -1){
-          free(temp);
-          return NULL;
-      }
-  }
-  
-  for(i=0; i < message_size/8; i++){
-    decrypted[i] = temp[i];
-  }
-  free(temp);
-  return decrypted;
-}
-
-int main(int argc, char **argv)
-{
-  struct public_key_class pub[1];
-  struct private_key_class priv[1];
-  rsa_gen_keys(pub, priv, PRIME_SOURCE_FILE);
-
-  printf("Private Key:\n Modulus: %lld\n Exponent: %lld\n", (long long)priv->modulus, (long long) priv->exponent);
-  printf("Public Key:\n Modulus: %lld\n Exponent: %lld\n", (long long)pub->modulus, (long long) pub->exponent);
-  
-  char message[] = "123abc";
-  int i;
-
-  printf("Original:\n");
-  for(i=0; i < strlen(message); i++){
-    printf("%lld\n", (long long)message[i]);
-  }  
-  
-  long long *encrypted = rsa_encrypt(message, sizeof(message), pub);
-  if (!encrypted){
-    fprintf(stderr, "Error in encryption!\n");
-    return 1;
-  }
-  printf("Encrypted:\n");
-  for(i=0; i < strlen(message); i++){
-    printf("%lld\n", (long long)encrypted[i]);
-  }  
-  
-  char *decrypted = rsa_decrypt(encrypted, 8*sizeof(message), priv);
-  if (!decrypted){
-    fprintf(stderr, "Error in decryption!\n");
-    return 1;
-  }
-  printf("Decrypted:\n");
-  for(i=0; i < strlen(message); i++){
-    printf("%lld\n", (long long)decrypted[i]);
-  }  
-  
-  printf("\n");
-  free(encrypted);
-  free(decrypted);
+  scanf("%s",msg);
+  for(i = 0; msg[i] != NULL; i++)
+    m[i] = msg[i];
+  n = x * y;
+  t = (x-1) * (y-1);
+  encryption_key();
+  printf("\nPOSSIBLE VALUES OF e AND d ARE\n");
+  for(i = 0; i < j-1; i++)
+    printf("\n%ld\t%ld", e[i], d[i]);
+  encrypt();
+  decrypt();
   return 0;
+}
+int prime(long int pr)
+{
+  int i;
+  j = sqrt(pr);
+  for(i = 2; i <= j; i++)
+  {
+   if(pr % i == 0)
+     return 0;
+  }
+  return 1;
+ }
+
+//function to generate encryption key
+void encryption_key()
+{
+  int k;
+  k = 0;
+  for(i = 2; i < t; i++)
+  {
+    if(t % i == 0)
+     continue;
+    flag = prime(i);
+    if(flag == 1 && i != x && i != y)
+    {
+     e[k] = i;
+     flag = cd(e[k]);
+    if(flag > 0)
+    {
+     d[k] = flag;
+     k++;
+    }
+   if(k == 99)
+    break;
+   }
+ }
+}
+long int cd(long int a)
+{
+  long int k = 1;
+  while(1)
+  {
+    k = k + t;
+    if(k % a == 0)
+     return(k / a);
+  }
+}
+
+//function to encrypt the message
+void encrypt()
+{
+  long int pt, ct, key = e[0], k, len;
+  i = 0;
+  len = strlen(msg);
+  while(i != len)
+  {
+    pt = m[i];
+    pt = pt - 96;
+    k = 1;
+    for(j = 0; j < key; j++)
+    {
+     k = k * pt;
+     k = k % n;
+    }
+   temp[i] = k;
+   ct = k + 96;
+   en[i] = ct;
+   i++;
+  }
+  en[i] = -1;
+  printf("\n\nTHE ENCRYPTED MESSAGE IS\n");
+  for(i = 0; en[i] != -1; i++)
+    printf("%c", en[i]);
+}
+
+//function to decrypt the message
+void decrypt()
+{
+  long int pt, ct, key = d[0], k;
+  i = 0;
+  while(en[i] != -1)
+  {
+    ct = temp[i];
+    k = 1;
+    for(j = 0; j < key; j++)
+    {
+      k = k * ct;
+      k = k % n;
+    }
+   pt = k + 96;
+   m[i] = pt;
+   i++;
+  }
+  m[i] = -1;
+  printf("\n\nTHE DECRYPTED MESSAGE IS\n");
+  for(i = 0; m[i] != -1; i++)
+   printf("%c", m[i]);
+  printf("\n");
 }
